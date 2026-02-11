416a94891f3917beb30b0e279d7fb84da0cf6983
vva-axelor <v.vaghela@axelor.com>
2026-02-11 16:23:05 +0530

    Anomaly RM#80143:Refactor LeaveController

--- axelor-human-resource/src/main/java/com/axelor/apps/hr/module/HumanResourceModule.java
+++ axelor-human-resource/src/main/java/com/axelor/apps/hr/module/HumanResourceModule.java
@@ -190,6 +190,8 @@ import com.axelor.apps.hr.service.extra.hours.ExtraHoursService;
 import com.axelor.apps.hr.service.extra.hours.ExtraHoursServiceImpl;
 import com.axelor.apps.hr.service.leave.IncrementLeaveService;
 import com.axelor.apps.hr.service.leave.IncrementLeaveServiceImpl;
+import com.axelor.apps.hr.service.leave.LeaveBusinessService;
+import com.axelor.apps.hr.service.leave.LeaveBusinessServiceImpl;
 import com.axelor.apps.hr.service.leave.LeaveExportService;
 import com.axelor.apps.hr.service.leave.LeaveExportServiceImpl;
 import com.axelor.apps.hr.service.leave.LeaveLineService;
@@ -228,6 +230,8 @@ import com.axelor.apps.hr.service.leave.LeaveRequestValidateService;
 import com.axelor.apps.hr.service.leave.LeaveRequestValidateServiceImpl;
 import com.axelor.apps.hr.service.leave.LeaveValueProrataService;
 import com.axelor.apps.hr.service.leave.LeaveValueProrataServiceImpl;
+import com.axelor.apps.hr.service.leave.LeaveViewService;
+import com.axelor.apps.hr.service.leave.LeaveViewServiceImpl;
 import com.axelor.apps.hr.service.leave.compute.LeaveRequestComputeDayDurationService;
 import com.axelor.apps.hr.service.leave.compute.LeaveRequestComputeDayDurationServiceImpl;
 import com.axelor.apps.hr.service.leave.compute.LeaveRequestComputeDurationService;
@@ -531,5 +535,7 @@ public class HumanResourceModule extends AxelorModule {
     bind(KilometricGoogleService.class).to(KilometricGoogleServiceImpl.class);
     bind(KilometricOsmService.class).to(KilometricOsmServiceImpl.class);
     bind(KilometricResponseToolService.class).to(KilometricResponseToolServiceImpl.class);
+    bind(LeaveViewService.class).to(LeaveViewServiceImpl.class);
+    bind(LeaveBusinessService.class).to(LeaveBusinessServiceImpl.class);
   }
 }
